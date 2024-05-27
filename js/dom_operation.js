4802902c3122b57491bf97c8b10baaa6e0bede7d
Qingyan <yangqingyan0@gmail.com>
2024-05-27 12:57:37 +0930

    feat: save work

---
+++ js/dom_operation.js
@@ -0,0 +1,7 @@
+const a = document.getElementById('idname');
+const b = document.getElementsByClassName('classname');// all ele with classname of classname
+const e = document.getElementsByTagName('div');// all divs
+const c = document.querySelector('div');//first div ele
+const g = document.querySelectorAll('div');
+const d = document.querySelector('#idname');
+const f = document.querySelector('.classname');// first
